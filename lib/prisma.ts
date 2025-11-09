ff8d9b178eef0fbb83412e7607f7bffe73feadc3
Felipe Rocha <felipemota.rocha@gmail.com>
2025-11-09 17:27:29 -0300

    chore: add PostgreSQL adapter and pg dependency to Prisma configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
 // lib/prisma.ts
 
 import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
